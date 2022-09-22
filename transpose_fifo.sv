d01e83204f3711d594144ac09853cf74a3415db3
garciacalder <garciacalder@ad.engr.wisc.edu>
2022-09-22 18:28:50 -0500

    adding transpose_fifo.sv

---
+++ transpose_fifo.sv
@@ -0,0 +1,39 @@
+module transpose_fifo  #(
+  parameter DEPTH=8,
+  parameter BITS=8
+  )
+  (
+  input clk,rst_n, en, Arow, WrEn,
+  input [BITS-1:0] Ain [DEPTH-1:0], 
+  output [BITS-1:0] Aout
+  );
+
+  logic [BITS-1:0] fifo [DEPTH-1:0];
+
+  always @(posedge clk, negedge rst_n) begin
+
+	if(!rst_n) begin
+		for(integer i = 0; i < DEPTH; i++) begin
+			fifo[i] <= '0; 
+		end
+	end
+	
+	else if(WrEn) begin
+		fifo <= Ain;
+	end
+
+	else if(en) begin
+		for(integer i = 0; i < DEPTH; i++) begin
+			if(i == DEPTH - 1)
+				fifo[DEPTH-1] <= '0;
+			else
+				fifo[i] <= fifo[i+1];
+		end
+	end
+	
+  end
+
+
+  assign Aout = fifo[0];
+
+endmodule
