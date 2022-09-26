75a58cb09d9f277dd54829666a487bd7375fd076
“Mark <07yhxiaxy@163.com>
2022-09-25 20:15:57 -0500

    still working, added comments to form structures

--- memA.sv
+++ memA.sv
@@ -1,3 +1,8 @@
+/*
+* memA module, which is a memory that stores the data as input to tpumac.
+* Takes input of 8 bit * 8 elements (which is a row)
+* outputs a 8 bit * 8 elements (which is a column)
+*/
 module memA
     #(
         parameter BITS_AB=8,
@@ -10,18 +15,19 @@ module memA
         output signed [BITS_AB-1:0] Aout [DIM-1:0]
     );
 
-    logic signed [BITS_AB-1:0] Ain_tmp [DIM-1:0] [DIM-1:0];
+    // Take the element coming out of each FIFO buffer as output of this module
+    logic signed [BITS_AB-1:0] Aout_tmp [DIM-1:0];
+    // write only to the row selected by Arow
     logic write;
-
+    // generate a list of transpose fifo modules that pads zeroes to put the data into rhombus shape
     genvar row, col;
     generate
         for (row=0; row<DIM; ++row) begin
             assign write = (Arow == row) ? WrEn : 0;
-            transpose_fifo tf(.clk(clk),.rst_n(rst_n),.en(en),.WrEn(write),.Ain(Ain[row]),.Aout(Aout[row][0]));
-            
+            transpose_fifo tf(.clk(clk),.rst_n(rst_n),.en(en),.WrEn(write),.Ain(Ain[row]),.Aout(Aout_tmp[row]));
         end
     endgenerate
 
-    // transpose_fifo
+    assign Aout = Aout_tmp;
 
 endmodule
--- transpose_fifo.sv
+++ transpose_fifo.sv
@@ -4,24 +4,26 @@ module transpose_fifo  #(
 )
 (
 	input clk,rst_n, en, WrEn,
+	input [$clog2(DEPTH)-1:0] Arow,
 	input [BITS-1:0] Ain [DEPTH-1:0], 
 	output [BITS-1:0] Aout
 );
 
   logic [BITS-1:0] fifo [DEPTH-1:0];
 
+// Make 8 FIFO modules each has 8 bits to store matrix data which will later be pumped to delayFIFO
   always @(posedge clk, negedge rst_n) begin
-
+	// Reset the fifo modules on negative reset
 	if(!rst_n) begin
 		for(integer i = 0; i < DEPTH; i++) begin
 			fifo[i] <= '0; 
 		end
 	end
-	
+	// Parallel write 8 bits data into fifo at WrEn
 	else if(WrEn) begin
 		fifo <= Ain;
 	end
-
+	// If enabled, pipeline the data to delayFIFO which appends zeroes to form rhombus shape.
 	else if(en) begin
 		for(integer i = 0; i < DEPTH; i++) begin
 			if(i == DEPTH - 1)
@@ -33,50 +35,14 @@ module transpose_fifo  #(
 	
   end
 
+  genvar row, col;
+  generate
+	for(row=0; row<BITS; ++row) begin
+		fifo df1(.clk(clk), .rst_n(rst_n), .en(en), .d(fifo[row]), .q());
+  	end
+  endgenerate
+
 
   assign Aout = fifo[0];
 
 endmodule
-
-// module transpose_fifo  #(
-//   parameter DEPTH=8,
-//   parameter BITS=8
-//   )
-//   (
-// 	input clk,rst_n, en, WrEn,
-// 	input [BITS-1:0] Ain [DEPTH-1:0], 
-// 	output [BITS-1:0] Aout
-//   );
-
-//   logic [BITS-1:0] fifo [DEPTH-1:0];
-
-//   genvar row, col;
-//   generate
-// 	for(col=0; col<BITS; ++col) begin
-// 		fifo[col] = WrEn? Ain[col] : 0;
-// 	end
-//   endgenerate
-//   always @(posedge clk, negedge rst_n) begin
-
-// 	if(!rst_n) begin
-// 		for(integer i = 0; i < DEPTH; i++) begin
-// 			fifo[i] <= '0; 
-// 		end
-// 	end
-
-// 	else if(en) begin
-// 		for(integer i = 0; i < DEPTH; i++) begin
-// 			if(i == DEPTH - 1)
-// 				fifo[DEPTH-1] <= '0;
-// 			else
-// 				fifo[i] <= fifo[i+1];
-// 		end
-// 	end
-	
-//   end
-  
-
-
-//   assign Aout = fifo[0];
-
-// endmodule
