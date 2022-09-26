e9036d7f25f918e251e34e3ea5a2db93c16a9a4e
“Mark <07yhxiaxy@163.com>
2022-09-25 20:45:52 -0500

    modified FIFO structure, still working on adding delayFIFO

--- transpose_fifo.sv
+++ transpose_fifo.sv
@@ -9,40 +9,47 @@ module transpose_fifo  #(
 	output [BITS-1:0] Aout
 );
 
-  logic [BITS-1:0] fifo [DEPTH-1:0];
+// DelayFIFO that holds zeroes to form a rhombus shape for FIFO
+logic [BITS-1:0] delay_fifo [DEPTH-1:0] [DEPTH-1:0];
 
-// Make 8 FIFO modules each has 8 bits to store matrix data which will later be pumped to delayFIFO
-  always @(posedge clk, negedge rst_n) begin
+// Make 8 FIFO modules each has 8 bits * 8 elements to store matrix data which will later be pumped to delayFIFO
+logic [BITS-1:0] fifo [DEPTH-1:0] [DEPTH-1:0];
+always @(posedge clk, negedge rst_n) begin
 	// Reset the fifo modules on negative reset
 	if(!rst_n) begin
 		for(integer i = 0; i < DEPTH; i++) begin
-			fifo[i] <= '0; 
+			for (integer j = 0; j < DEPTH; j++) begin
+				fifo[i][j] <= '0;
+			end
 		end
 	end
-	// Parallel write 8 bits data into fifo at WrEn
+	// Parallel write 8 bits data into fifo with corresponding rows at signal WrEn
 	else if(WrEn) begin
-		fifo <= Ain;
+		for (integer row = 0; row < DEPTH; row++) begin
+			if (row == Arow) begin
+				fifo[row] <= Ain;
+			end
+		end
 	end
 	// If enabled, pipeline the data to delayFIFO which appends zeroes to form rhombus shape.
 	else if(en) begin
 		for(integer i = 0; i < DEPTH; i++) begin
-			if(i == DEPTH - 1)
+			if (i == DEPTH - 1)
 				fifo[DEPTH-1] <= '0;
 			else
 				fifo[i] <= fifo[i+1];
 		end
 	end
-	
-  end
+end
 
-  genvar row, col;
-  generate
-	for(row=0; row<BITS; ++row) begin
-		fifo df1(.clk(clk), .rst_n(rst_n), .en(en), .d(fifo[row]), .q());
-  	end
-  endgenerate
+genvar row, col;
+generate
+for(row=0; row<BITS; ++row) begin
+	fifo df1(.clk(clk), .rst_n(rst_n), .en(en), .d(fifo[row]), .q());
+end
+endgenerate
 
 
-  assign Aout = fifo[0];
+assign Aout = fifo[0];
 
 endmodule
