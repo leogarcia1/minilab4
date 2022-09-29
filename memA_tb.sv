a54e683f59fdc33113f880d6a6810fd7c3d8fbc5
Mark Xia <07yhxiaxy@163.com>
2022-09-29 09:33:48 -0500

    modified memAtb

--- memA_tb.sv
+++ memA_tb.sv
@@ -1,74 +1,102 @@
 // memA test bench
 
-`include "systolic_array_tc.svh"
+`include "memA_tc.svh"
 
 module memA_tb();
 
     localparam BITS_AB=8;
-    localparam BITS_C=16;
     localparam DIM=8;
     localparam ROWBITS=$clog2(DIM);
-
-    localparam TESTS=16;
+    localparam TESTS=1;
 
     // Clock
     logic clk;
     logic rst_n;
     logic en;
     logic WrEn;
+    // Signal that selects which row the tb will write to
     logic [ROWBITS-1:0] Arow;
-    logic signed [BITS_AB-1:0] A [DIM-1:0];
-    logic signed [BITS_C-1:0] Aout [DIM-1:0];
+    // Raw input values which will be pumped into the memA module
+    logic signed [BITS_AB-1:0] A [DIM-1:0][DIM-1:0];
+    // Input rows which will be pumped into the memA module
+    logic signed [BITS_AB-1:0] Ain [DIM-1:0];
+    // A matrix that stores the giant square shape data which contains a rhombus shaped A
+    logic signed [BITS_AB-1:0] Aout [DIM-1:0][3*DIM-2-1:0];
+    // Temporary value that stores the value coming out of memA
+    logic signed [BITS_AB-1:0] Aout_tmp [DIM-1:0];
 
+    // Generate clock
     always #5 clk = ~clk;
 
-    memA iDUT(.clk(clk), .rst_n(rst_n), .en(en), .WrEn(WrEn), .Ain(A), .Arow(Crow), .Aout(Aout));
+    // Keep track of how many errors and which cycle is the tb in
+    integer errors, mycycle;
 
-    integer errors,mycycle;
-
-    systolic_array #(.BITS_AB(BITS_AB),
-                    .BITS_C(BITS_C),
-                    .DIM(DIM)
-                    ) DUT (.*);
-
-    systolic_array_tc #(.BITS_AB(BITS_AB),
-                        .BITS_C(BITS_C),
-                        .DIM(DIM)
-                        ) satc;
-
-    // register Cout values
-    always @(posedge clk) begin
-        Coutreg <= Cout;
-    end
+    // Instantiate the device under test
+    memA #(.BITS_AB(BITS_AB), .DIM(DIM)) iDUT(.clk(clk), .rst_n(rst_n), .en(en), .WrEn(WrEn), .Ain(Ain), .Arow(Arow), .Aout(Aout_tmp));
+    memA_tc #(.BITS_AB(BITS_AB), .DIM(DIM)) matc;
 
     initial begin
+        // Instantiate the testing case
+        matc = new();
         clk = 1'b0;
         rst_n = 1'b1;
+        mycycle = 0;
+        // Hold the memA from operating until later stage after all values are present in memA
         en = 1'b0;
         WrEn = 1'b0;
         errors = 0;
-        Crow = {ROWBITS{1'b0}};
-        for(int rowcol=0;rowcol<DIM;++rowcol) begin
-            A[rowcol] = {BITS_AB{1'b0}};
-            B[rowcol] = {BITS_AB{1'b0}};
-            Cin[rowcol] = {BITS_C{1'b0}};
-        end
+        Arow = {ROWBITS{1'b0}};
+        //----------- INITIALIZE BLOCK: Initialize values in different matrices to 0 ------------
         // reset and check Cout
         @(posedge clk) begin end
         rst_n = 1'b0; // active low reset
         @(posedge clk) begin end
         rst_n = 1'b1; // reset finished
-        @(posedge clk) begin end
-
-        // check that C was properly reset
-        for(int Row=0;Row<DIM;++Row) begin
-            Crow = {Row[ROWBITS-1:0]};
-            @(posedge clk) begin end
-            for(int Col=0;Col<DIM;++Col) begin
-            if(Coutreg[Col] !== 0) begin
-                    errors++;
-                    $display("Error! Reset was not conducted properly. Expected: 0, Got: %d for Row %d Col %d", Coutreg[Col],Row, Col); 
+        @(posedge clk) begin
+            // Initialize values in different matrices to 0
+            for(int rowcol=0;rowcol<DIM;++rowcol) begin
+                A[rowcol] = {BITS_AB{1'b0}};
+                Aout_tmp[rowcol] = {BITS_AB{1'b0}};
+            end
+            for(int row=0; row<DIM; row++) begin
+                for(int col=0; col<DIM; col++) begin
+                    Aout[row][col] = {BITS_AB{1'b0}};
                 end
             end
         end
+        //----------- END INITIALIZE BLOCK: Initialize values in different matrices to 0 ------------
+        //----------- TESTING BLOCK: test memA behavior MULTIPLE TIMES using different random value matrices ------------
+        for(int test=0; test<TESTS; test++)begin
+            A = matc.new_matrix();  // Generate new random values to test memA
+            // Start writing values into the memA module
+            @(negedge clk) begin
+                WrEn = 1'b1;
+            end
+            // Load values from register into memA that will be tested
+            @(posedge clk) begin
+                for(int i=0; i<DIM; ++i)begin
+                    @(posedge clk)begin
+                        Arow = i;
+                    end
+                end
+            end
+            // Stop writing into the memA; start letting memA shifting values out of it.
+            @(negedge clk) begin
+                WrEn = 1'b0;
+                en = 1'b1;  // enables the memA, memA should start shifting out values
+            end
+            @(posedge clk) begin
+                for(int col=0; col<DIM; ++col)begin
+                    for(int row=0; row<DIM; ++row)begin
+                        Aout[row][mycycle] = Aout_tmp[row];
+                    end
+                    @(posedge clk) begin
+                        mycycle = matc.next_cycle();
+                    end
+                end
+            end
+            // TODO: Dump it to visualize it
+            matc.dump(Aout);
+        end
     end
+endmodule
